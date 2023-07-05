64a70b468ca5870aa2bd03dd3a362a46f5bd06b4
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-05 12:46:58 +0200

    added first validation

--- validator.py
+++ validator.py
@@ -1,4 +1,7 @@
 def validate_password(password):
+    if len(password) < 8:
+        return ("Password must be at least 8 characters long")
+
     return "Password is valid!"
 
 password = input("Please enter a password: ")
