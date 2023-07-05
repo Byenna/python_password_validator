5cab53ef27ffa14baec8eede256d209cd6499bce
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-05 12:52:40 +0200

    added lowercase validation

--- validator.py
+++ validator.py
@@ -3,6 +3,8 @@ def validate_password(password):
         return ("Password must be at least 8 characters long")
     if not any(char.isupper() for char in password):
         return("Password must contain at least one uppercase letter")
+    if not any(char.islower() for char in password):
+        return("Password must contain at least one lowercase letter")
     return "Password is valid!"
 
 password = input("Please enter a password: ")
