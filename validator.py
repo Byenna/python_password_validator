182be203660ec4fc0af6e5a5cdff67c327925824
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-05 12:50:30 +0200

    added upper validation

--- validator.py
+++ validator.py
@@ -1,7 +1,8 @@
 def validate_password(password):
     if len(password) < 8:
         return ("Password must be at least 8 characters long")
-
+    if not any(char.isupper() for char in password):
+        return("Password must contain at least one uppercase letter")
     return "Password is valid!"
 
 password = input("Please enter a password: ")
