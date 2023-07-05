012e055aa39748cbdfacede4675c2eb10b9fc3a4
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-05 12:54:32 +0200

    added digit validation

--- validator.py
+++ validator.py
@@ -5,6 +5,8 @@ def validate_password(password):
         return("Password must contain at least one uppercase letter")
     if not any(char.islower() for char in password):
         return("Password must contain at least one lowercase letter")
+    if not any(char.isdigit() for char in password):
+        return("Password must contain at least one digit")
     return "Password is valid!"
 
 password = input("Please enter a password: ")
